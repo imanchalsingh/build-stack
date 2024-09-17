cf4d900dcb4cfffa9437b33fde8a32d84a61f48e
anchalDev159357 <singhanchal159357@gmail.com>
2024-09-17 10:20:26 +0530

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+<<<<<<< HEAD
 import './App.css';
 import Projectpage from "./component/Projectpage"
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> f44a73f (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Projectpage/>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> f44a73f (Initialize project using Create React App)
     </div>
   );
 }
